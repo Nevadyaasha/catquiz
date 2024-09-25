33c1d85b8e17bac04caf51dfd2b4c48eac849fde
Nevadyaasha <nevadyaasha@gmail.com>
2024-09-25 20:43:04 +0300

    mid fixes

---
+++ questions.js
@@ -0,0 +1,8 @@
+let nextQuestion = document.querySelectorAll(".btn_nextquestion");
+
+// МАССИВ - добавить к каждому
+
+nextQuestion.addEventListener("click", function (event) {
+
+        window.moveTo(100,0)
+})
